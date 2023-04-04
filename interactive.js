27aa25ca4cab8bc1e656edb6cf0d684a5069701c
Brian <linbrian24@gmail.com>
2023-04-03 22:55:13 -0400

    Add submission feature for button

--- interactive.js
+++ interactive.js
@@ -1,23 +1,33 @@
-let rating = "";
-let active = "";
+let currRating = "";
+let selectedRating = "";
 
 const numbers = document.querySelectorAll(".number");
 const button = document.querySelector(".button");
+const userSelection = document.querySelector(".thanks__userSelection")
+const ratingScreen = document.querySelector(".rating");
+const thankYouScreen = document.querySelector(".thanks");
 
 numbers.forEach((num) => {
     num.addEventListener("click", () => {
         // Deselect previous number.
-        if (active !== "") {
-            active.classList.remove("select");
+        if (selectedRating !== "") {
+            selectedRating.classList.remove("select");
         }
-        
-        rating = num.textContent.trim();
+
+        currRating = num.textContent.trim();
         num.classList.add("select");
 
-        active = num;
+        selectedRating = num;
     }
 )});
 
 button.addEventListener("click", () => {
-    console.log(button);
+    // Make sure you can't select if rating isn't chosen.
+    if (currRating === "") {
+        return;
+    }
+
+    userSelection.textContent = currRating;
+    ratingScreen.classList.add("hide");
+    thankYouScreen.classList.remove("hide");
 });

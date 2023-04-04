60cda8e9258fc54c192992a11c256eca54674da1
Brian <linbrian24@gmail.com>
2023-04-03 23:18:30 -0400

    Organize JS code

--- interactive.js
+++ interactive.js
@@ -7,21 +7,19 @@ const userSelection = document.querySelector(".thanks__userSelection")
 const ratingScreen = document.querySelector(".rating");
 const thankYouScreen = document.querySelector(".thanks");
 
-numbers.forEach((num) => {
-    num.addEventListener("click", () => {
-        // Deselect previous number.
-        if (selectedRating !== "") {
-            selectedRating.classList.remove("select");
-        }
+const selectNumber = (num) => {
+    // Deselect previous number.
+    if (selectedRating !== "") {
+        selectedRating.classList.remove("select");
+    }
 
-        currRating = num.textContent;
-        num.classList.add("select");
+    currRating = num.textContent;
+    num.classList.add("select");
 
-        selectedRating = num;
-    })
-});
+    selectedRating = num;
+}
 
-button.addEventListener("click", () => {
+const submitRating = () => {
     // Make sure you can't select if rating isn't chosen.
     if (currRating === "") {
         return;
@@ -30,4 +28,8 @@ button.addEventListener("click", () => {
     userSelection.textContent = currRating;
     ratingScreen.classList.add("hide");
     thankYouScreen.classList.remove("hide");
-});
+}
+
+numbers.forEach((num) => {num.addEventListener("click", () => selectNumber(num))});
+
+button.addEventListener("click", submitRating);
